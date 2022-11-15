585b07398cb82415030b58b46361780d04c14216
hardlife0 <102363884+hardlife0@users.noreply.github.com>
2022-11-15 23:37:02 +0900

    Add files via upload

---
+++ hw.7.c
@@ -0,0 +1,36 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main(void)
+{
+    int arr1[6]={1,2,3,4,5,6}, arr2[6]={7,8,9,10,11,12};
+    int * ptr1= arr1;
+    int * ptr2= arr2;
+    int *temp, i;
+    printf("arr1: ");
+    for(i=0; i<6; i++)
+    {printf("%d,", arr1[i]);}
+    printf("\n");
+    printf("arr2: ");
+    for(i=0; i<6; i++)
+    {printf("%d,", arr2[i]);}
+    printf("\n");
+
+    for(i=0; i<6;i++)
+   {temp= *ptr1;
+    *ptr1= *ptr2;
+    *ptr1= temp;
+   }
+
+    printf("after swap \n");
+    printf("arr1: ");
+    for(i=0; i<6; i++)
+    {printf("%d,", arr1[i]);}
+    printf("\n");
+    printf("arr2: ");
+    for(i=0; i<6; i++)
+    {printf("%d,", arr2[i]);}
+    printf("\n");
+
+    return 0;
+}
